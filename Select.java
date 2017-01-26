90fd98bd9babbae044b6bd4a5ed39384022c4363
Jackson Myers <jackson@Jacksons-MacBook-Pro.local>
2017-01-26 00:46:34 -0800

    final commit

--- Select.java
+++ Select.java
@@ -1,11 +1,10 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
+
 
 public class Select {
-
     public static void main(String[] args) throws Exception {
-
-      	BufferedReader stdIn = new java.io.BufferedReader ( new java.io.InputStreamReader ( System.in ) );
+        BufferedReader stdIn = new java.io.BufferedReader ( new java.io.InputStreamReader ( System.in ) );
         String s = stdIn.readLine();// grab the first line (or null, if end-of-file)
         ArrayList<Integer> numList = new ArrayList<Integer>();
 
@@ -13,51 +12,53 @@ public class Select {
             numList.add(Integer.parseInt(s));
             s = stdIn.readLine(); // grab the next line (or null)
         }
-				findKthLargest(numList, Integer.parseInt(args[0]));
-		}
 
-    public static int findKthLargest (ArrayList<Integer> numList, int k) {
-			if (k < 1 || numList == null){
-				throw new IllegalArgumentException();
-			}
-
-			int pivotIndex = (int)(Math.random() * numList.size() -1);
-			swapAtIndex(numList, pivotIndex, numList.size() - 1);
-			pivotIndex = partition(numList, 0, numList.size() - 1);
-
-			if ((k-1) < pivotIndex) {
-
-				return findKthLargest(numList.sublist(0, pivotIndex), k);
-
-			} else if ((k-1) > pivotIndex) {
-
-				return findKthLargest(numList.sublist(pivotIndex + 1, numList.size() - 1), (k-1)-pivotIndex);
-
-			} else {
-				return numList.get(k-1);
-			}
-
+        System.out.println(findKthLargest(numList, Integer.parseInt(args[0])));
     }
 
-		private static int partition (ArrayList<Integer> numList, int begin, int end) {
-          int pivotVal = numList.get(end),
-              swapIndex  = begin;
+    public static int findKthLargest (List<Integer> numList, int k) {
+        if (k < 1 || numList == null || k > numList.size() ) {
+            throw new IllegalArgumentException();
+        }
 
-          for (int j = begin; j < end; j++) {
-              if (numList.get(j) <= pivotVal) {
-                  swapAtIndex(numList, swapIndex, j);
-                  swapIndex++;
-              }
-          }
+        int pivotIndex = (int)(Math.random() * numList.size());
+        //System.out.println("; " + pivotIndex);
+        swap(numList, pivotIndex, numList.size() - 1);
+        pivotIndex = partition(numList, 0, numList.size() - 1);
 
-          swapAtIndex(numList, swapIndex, end);
-          return swapIndex;
-      }
 
-    private static void swapAtIndex (ArrayList<Integer> arr, int p1, int p2) {
-        int temp = arr.get(p1);
-        arr.set(p1, arr.get(p2));
-        arr.set(p2, temp);
+        if ((k-1) < pivotIndex) {
+
+            return findKthLargest ( numList.subList(0, pivotIndex) , k );
+
+        } else if ((k-1) > pivotIndex) {
+
+            return findKthLargest(  numList.subList(pivotIndex + 1, numList.size()) , (k - 1 - pivotIndex) );
+
+        } else {//equal
+
+            return numList.get((k-1));
+        }
+    }
+
+    public static int partition(List<Integer> numList, int begin, int end) { //returns the index of where the pivot should be
+        int pivotValue = numList.get(end);
+        int swapIndex = begin;
+
+        for (int i = begin; i < end; i++) {
+            if (numList.get(i) <= pivotValue) {
+                swap(numList, swapIndex, i);
+                swapIndex++;
+            }
+        }
+        swap(numList, swapIndex, end);
+        return swapIndex;
+    }
+
+    public static void swap(List<Integer> numList, int iOne, int iTwo) {
+        int temp = numList.get(iOne);
+        numList.set(iOne, numList.get(iTwo));
+        numList.set(iTwo, temp);
     }
 
 }
